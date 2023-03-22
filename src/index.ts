b1131365c7b618b79b5203bfc793cb2168b9e837
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-03-22 12:40:02 +0530

    feat: ✨ use `writeToCSV()`

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { polygon } from '@lens-protocol/client';
 import { Lens } from './lib/lens';
 import { logger } from './common/logger';
+import { writeToCSV } from './lib/csv';
 
 (async function () {
+  const handle = 'mcsquared.lens';
   const lens = new Lens(polygon);
   const allFollowerAddresses = await lens.getAllFollowersByHandleConcurrently(
-    'mcsquared.lens'
+    handle
   );
 
   logger.info('allFollowerAddresses: ', allFollowerAddresses);
+
+  const data = allFollowerAddresses.map((address) => ({
+    address
+  }));
+
+  writeToCSV('address', data, handle + '.csv');
 })();
