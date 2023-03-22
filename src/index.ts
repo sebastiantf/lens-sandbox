96713776f12ec22a99ce7386e64d13d5ba7e2ece
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-03-22 12:21:02 +0530

    feat: ✨ add `getAllFollowersByHandle()`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { polygon } from '@lens-protocol/client';
 import { Lens } from './lib/lens';
+import { logger } from './common/logger';
 
 (async function () {
   const lens = new Lens(polygon);
+  const allFollowerAddresses = await lens.getAllFollowersByHandle(
+    'mcsquared.lens'
+  );
+
+  logger.info('allFollowerAddresses: ', allFollowerAddresses);
 })();
--- src/lib/lens.ts
+++ src/lib/lens.ts
@@ -1,5 +1,10 @@
-import LensClient from '@lens-protocol/client';
+import LensClient, {
+  FollowerFragment,
+  PaginatedResult
+} from '@lens-protocol/client';
 import { Environment } from '@lens-protocol/client/dist/declarations/src/consts/environments';
+import assert from 'assert';
+import { logger } from '../common/logger';
 
 export class Lens {
   protected lensClient: LensClient;
@@ -8,5 +13,45 @@ export class Lens {
     this.lensClient = new LensClient({
       environment
     });
+  }
+
+  public async getAllFollowersByHandle(handle: string) {
+    // get lens profile by handle
+    const profileByHandle = await this.lensClient.profile.fetch({
+      handle
+    });
+    const profileId = profileByHandle?.id ?? '';
+    logger.info('profileId: ', profileId);
+    logger.info('handle: ', handle);
+
+    // init all results
+    const allResults: FollowerFragment[] = [];
+    // init first page of results
+    const firstPage = await this.lensClient.profile.allFollowers({ profileId });
+
+    // total results count
+    const totalFollowers = firstPage.pageInfo.totalCount ?? 0;
+    logger.info('totalFollowers: ', totalFollowers);
+
+    // loop as long as result != null
+    let page = 1;
+    let nextResult: PaginatedResult<FollowerFragment> | null = firstPage;
+    while (nextResult) {
+      allResults.push(...nextResult.items);
+      logger.debug(`Fetched page ${page++}`);
+
+      nextResult = await nextResult.next();
+    }
+    logger.info('allResults.length:', allResults.length);
+    assert.equal(totalFollowers, allResults.length);
+
+    // extract addresses alone
+    const allFollowerAddresses = allResults.map(
+      (result) => result.wallet.address
+    );
+    logger.info('allFollowerAddresses.length: ', allFollowerAddresses.length);
+    assert.equal(allFollowerAddresses.length, allResults.length);
+
+    return allFollowerAddresses;
   }
 }
